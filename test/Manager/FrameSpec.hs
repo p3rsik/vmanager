5023ecc5829cce6241d9975d35588d6bd190556a
p3rsik <yroslav541@gmail.com>
2020-10-02 17:22:51 +0300

    Add -getFrame- tests

--- test/Manager/FrameSpec.hs
+++ test/Manager/FrameSpec.hs
@@ -1,21 +1,44 @@
+{-# LANGUAGE DataKinds #-}
+
 module Manager.FrameSpec (spec) where
 
 import Foundation
+import Foundation.Collection
+import Control.Carrier.State.Strict
+import Control.Algebra
 import Test.Hspec
-import Test.QuickCheck
+import qualified Test.QuickCheck as Q
+import qualified Test.QuickCheck.Monadic as QM
 import Manager.Types
 import Manager.Frame
 
-idToOffToId :: NonNegative Int -> Bool
-idToOffToId (NonNegative x) = (idToOff . offToId $ Offset x) == (Offset x)
+idToOffToId :: Q.NonNegative Int -> Bool
+idToOffToId (Q.NonNegative x) = (idToOff . offToId $ Offset x) == (Offset x)
 
-offToIdToOff :: NonNegative Int -> Bool
-offToIdToOff (NonNegative x) = (offToId . idToOff $ Fid x) == (Fid x)
+offToIdToOff :: Q.NonNegative Int -> Bool
+offToIdToOff (Q.NonNegative x) = (offToId . idToOff $ Fid x) == (Fid x)
+
+frameNumber = 32
+
+startingState :: FrameTable
+startingState = FT rfo ram sfo sw where
+    rfo = [Offset x | x <- [0..frameNumber]]
+    ram = nonEmpty_ [Frame (Fid x) [0..64] | x <- [0..frameNumber]]
+    sfo = [Offset x | x <- [0..frameNumber]]
+    sw = nonEmpty_ [Frame (Fid x) [0..64] | x <- [0..frameNumber]]
+
+run' = run . evalState startingState 
+
+getFrameTest :: Q.Property
+getFrameTest = Q.forAll (Q.choose (0, frameNumber)) $ \x ->
+    let res = run' $ getFrame (Fid x :: FrameId 'Ram)
+        (FT _ ram _ _) = startingState
+        res' = nonEmpty_ . filter (\f -> x == (unFid $ frameId f)) $ getNonEmpty ram
+        in res == head res'
 
 spec :: Spec
 spec = do 
     describe "idToOff and offToId roundabout" $ do 
-        it "idToOff . offToId == id" $ 
-            property $ idToOffToId
-        it "offToId . idToOff == id" $
-            property $ offToIdToOff
+        it "idToOff . offToId == id" $ Q.property $ idToOffToId
+        it "offToId . idToOff == id" $ Q.property $ offToIdToOff
+        it "getFrame tests" $ Q.property $ getFrameTest
